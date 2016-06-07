389bcb037070639c482a5e6e0a7d80690268fc69
Mame Medoune Diop <medoune.diop@qualshore.com>
2016-06-07 17:05:20 +0000

    remove thing endpoint, add game endpoint

--- server/api/game/game.controller.js
+++ server/api/game/game.controller.js
@@ -1,16 +1,16 @@
 /**
  * Using Rails-like standard naming convention for endpoints.
- * GET     /api/things              ->  index
- * POST    /api/things              ->  create
- * GET     /api/things/:id          ->  show
- * PUT     /api/things/:id          ->  update
- * DELETE  /api/things/:id          ->  destroy
+ * GET     /api/games              ->  index
+ * POST    /api/games              ->  create
+ * GET     /api/games/:id          ->  show
+ * PUT     /api/games/:id          ->  update
+ * DELETE  /api/games/:id          ->  destroy
  */
 
 'use strict';
 
 import _ from 'lodash';
-import Thing from './thing.model';
+import Game from './game.model';
 
 function respondWithResult(res, statusCode) {
   statusCode = statusCode || 200;
@@ -59,43 +59,43 @@ function handleError(res, statusCode) {
   };
 }
 
-// Gets a list of Things
+// Gets a list of Games
 export function index(req, res) {
-  return Thing.find().exec()
+  return Game.find().exec()
     .then(respondWithResult(res))
     .catch(handleError(res));
 }
 
-// Gets a single Thing from the DB
+// Gets a single Game from the DB
 export function show(req, res) {
-  return Thing.findById(req.params.id).exec()
+  return Game.findById(req.params.id).exec()
     .then(handleEntityNotFound(res))
     .then(respondWithResult(res))
     .catch(handleError(res));
 }
 
-// Creates a new Thing in the DB
+// Creates a new Game in the DB
 export function create(req, res) {
-  return Thing.create(req.body)
+  return Game.create(req.body)
     .then(respondWithResult(res, 201))
     .catch(handleError(res));
 }
 
-// Updates an existing Thing in the DB
+// Updates an existing Game in the DB
 export function update(req, res) {
   if (req.body._id) {
     delete req.body._id;
   }
-  return Thing.findById(req.params.id).exec()
+  return Game.findById(req.params.id).exec()
     .then(handleEntityNotFound(res))
     .then(saveUpdates(req.body))
     .then(respondWithResult(res))
     .catch(handleError(res));
 }
 
-// Deletes a Thing from the DB
+// Deletes a Game from the DB
 export function destroy(req, res) {
-  return Thing.findById(req.params.id).exec()
+  return Game.findById(req.params.id).exec()
     .then(handleEntityNotFound(res))
     .then(removeEntity(res))
     .catch(handleError(res));
